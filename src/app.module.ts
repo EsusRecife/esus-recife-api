3018988d11bffe7d14b08d580a6b60e23fc051b5
Marcus Lourenco <marcus.lourenco@zrobank.com.br>
2023-05-03 22:07:51 -0300

    feat: Added database connection to app.module; Added educational_institutions controllers and providers to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
+require('dotenv').config({ path: __dirname + '/./../.env' });
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { EducationalInstitution } from './models/educational_institution.model';
+import { EducationalInstitutionService } from './services/educational_institution.service';
+import { EducationalInstitutionController } from './controllers/educational_institution.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([EducationalInstitution]),
+  ],
+  controllers: [EducationalInstitutionController],
+  providers: [
+    EducationalInstitutionService,
+    {
+      provide: 'Educational_Institution_Repository',
+      useValue: EducationalInstitution,
+    },
+  ],
 })
 export class AppModule {}
