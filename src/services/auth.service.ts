19921ce4879c491783a2d8a71ed842262825b524
Marcus Vinícius Santana Lourenço <94205248+mav273@users.noreply.github.com>
2023-05-06 14:34:29 +0000

    feat: Added AuthService

---
+++ src/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Auth } from 'src/models/auth.model';
+import { EducationalInstitutionService } from './educational_institution.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private educationalInstitutionService: EducationalInstitutionService,
+    private jwtService: JwtService
+  ) {}
+
+  async signIn(user: Auth) {
+    const userId = await this.educationalInstitutionService.findById(user.inepCod);
+    if (!userId) {
+        throw new UnauthorizedException('O código inep digitado não foi encontrado no nosso banco de dados');
+    }
+
+    const isMatch = await bcrypt.compare(user.password, userId.password);
+    if (!isMatch) {
+        throw new UnauthorizedException('Senha Incorreta');
+    }
+
+    const payload = { username: user.inepCod };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
+
