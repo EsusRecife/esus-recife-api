1ac46ae835b24f631b925b478e7e9500adcf8eb8
Marcus Vinícius Santana Lourenço <94205248+mav273@users.noreply.github.com>
2023-05-06 14:34:07 +0000

    feat: AuthModel

---
+++ src/models/auth.model.ts
@@ -0,0 +1,11 @@
+import { Model, Column, Table } from 'sequelize-typescript';
+
+// @Table
+// export class Auth extends Model<Auth> {
+//   @Column
+//   inepCod: number;
+
+//   @Column
+//   password: string;
+
+// }
