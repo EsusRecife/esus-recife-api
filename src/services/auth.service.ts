445be9925884d9166c37f020df4f543cf9b197c0
Marcus Vinícius Santana Lourenço <94205248+mav273@users.noreply.github.com>
2023-05-10 13:56:59 +0000

    chore: Updated userId function; Updated payload

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -13,17 +13,17 @@ export class AuthService {
   ) {}
 
   async signIn(user: Auth) {
-    const userId = await this.educationalInstitutionService.findById(user.inepCod);
+    const userId = await this.educationalInstitutionService.findByInepCod(user.inepCod);
     if (!userId) {
         throw new UnauthorizedException('O código inep digitado não foi encontrado no nosso banco de dados');
     }
 
-    const isMatch = await bcrypt.compare(user.password, userId.password);
+    const isMatch = await bcrypt.compare(String(user.password), userId.password);
     if (!isMatch) {
         throw new UnauthorizedException('Senha Incorreta');
     }
 
-    const payload = { username: user.inepCod };
+    const payload = { userId: user.id };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
