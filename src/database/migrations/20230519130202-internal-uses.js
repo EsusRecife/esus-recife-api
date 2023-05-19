3f5fdfbcdac1b9c29beb565033c879fd7f61310b
Marcus Vinícius Santana Lourenço <94205248+mav273@users.noreply.github.com>
2023-05-19 13:38:57 +0000

    feat: Added internal-use migration and seeders

---
+++ src/database/migrations/20230519130202-internal-uses.js
@@ -0,0 +1,49 @@
+'use strict';
+const {v4:uuidv4} = require('uuid') 
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('InternalUses', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: uuidv4()
+      },
+      activity: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      qtyStudent: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      qtyEducator: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      infoStudent: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      materials: {
+        type: Sequelize.JSONB,
+        allowNull: true
+      },
+      inepCod: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        defaultValue: new Date(),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: new Date(),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('InternalUses');
+  }
+};
