325cb6a757e847bf8299edd79b1919de5cacb056
Marcus Lourenco <marcus.lourenco@zrobank.com.br>
2023-05-26 17:46:23 -0300

    feat: Added CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './nest/app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    methods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: [
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
+    ],
+  });
+
   await app.listen(3000);
 }
 main();
